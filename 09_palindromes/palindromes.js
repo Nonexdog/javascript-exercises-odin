cb200ef6fa4abdbe98d0fb11f96bc6a7ac7e25b7
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-31 01:09:40 -0300

    Add function body

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const reversedString = string.split('').reverse().join('');
+  return string === reversedString;
 };
 
+console.log(palindromes('racecar'));
+console.log(palindromes('tacos'));
+
 // Do not edit below this line
-module.exports = palindromes;
+// module.exports = palindromes;
